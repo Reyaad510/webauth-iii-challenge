66119ba843913d54bc3d9770e01a86ccdc5839f3
Reyaad Rafeek <reyaad510@gmail.com>
2019-06-13 21:58:40 -0400

    Created Login.js, api.js, Signup.js(scaffold), users.js(scaffold)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
+import Users from './Users';
 
 
-function App() {
+class App extends React.Component {
+
+
+
+  render() {
   return (
-    <h1> Howdy! :)</h1>
+    <div>
+
+      <h1> Howdy There! :)</h1>
+
+      <ul>
+        <li><NavLink to='/login'>Login</NavLink></li>
+        <li><NavLink to='/signup'>Signup</NavLink></li>
+        <li><NavLink to='/users'>Users</NavLink></li>
+        <li><button>Logout</button></li>
+      </ul>
+
+      <div>
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={Signup} />
+        <Route path='/users' component={Users} />
+      </div>
+
+    </div>
     )
+  }
 }
 
-export default App;
+export default withRouter(App);
